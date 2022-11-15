18da6842b2ef8c3752cb468f962a3f38978d9ffc
brunomarques97 <116980642+brunomarques97@users.noreply.github.com>
2022-11-15 15:28:35 -0300

    Adicionando botão

--- formulario1.js
+++ formulario1.js
@@ -1,6 +1,9 @@
 const url="https://api.adviceslip.com/advice"
-fetch(url).then((x)=>
+const mensagem=document.querySelector('#gerador')
+mensagem.addEventListener('click',()=>{
+    fetch(url).then((x)=>
     x.json()
     ).then((y)=>
         document.getElementById('mensagem').innerText=y.slip.advice
     );
+})
