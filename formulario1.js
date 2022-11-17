bb96fff73d3ef5d410866cde4fafa6bb59ce98da
brunomarques97 <116980642+brunomarques97@users.noreply.github.com>
2022-11-17 17:53:04 -0300

    Sortear um numero para o id

--- formulario1.js
+++ formulario1.js
@@ -1,12 +1,12 @@
-const url="https://api.adviceslip.com/advice/2"
+const min=1
+const max= 501
+let numero= Math.floor(Math.random() * (max-min))+min
 const proximo=document.querySelector('#proximo')
 mensagem.addEventListener('click',()=>{})
+console.log(numero)
 
-
-
-//document.getElementById('proximo').innerText=y.slip.advice
-fetch("https://api.adviceslip.com/advice/2").then((promise)=>
-    promise.json()
+fetch("https://api.adviceslip.com/advice").then((promise)=>
+    promise.json())
     .then((y)=>
-    console.log(y.slip.advice))
+    document.getElementById('mensagem').innerText=y.slip.advice
     );
