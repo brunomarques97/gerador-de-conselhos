eb2e4d735e726aa6dd67ad26dc24a6795064b3d3
brunomarques97 <116980642+brunomarques97@users.noreply.github.com>
2022-11-17 20:37:33 -0300

    Configurando botao proximo e anterior

--- formulario1.js
+++ formulario1.js
@@ -1,12 +1,31 @@
 const min=1
-const max= 501
+const max= 101
 let numero= Math.floor(Math.random() * (max-min))+min
 const proximo=document.querySelector('#proximo')
-mensagem.addEventListener('click',()=>{})
-console.log(numero)
+const anterior=document.querySelector('#anterior')
+const copiar=document.querySelector('#copiar')
 
-fetch("https://api.adviceslip.com/advice").then((promise)=>
+fetch("https://api.adviceslip.com/advice/"+ numero).then((promise)=>
+    promise.json()).then((y)=>    
+    document.getElementById('mensagem').innerText=y.slip.advice
+    );
+
+proximo.addEventListener('click',()=>{
+    let proximoID = numero+1
+
+    fetch("https://api.adviceslip.com/advice/"+ proximoID).then((promise)=>
+    promise.json()).then((y)=>
+    document.getElementById('mensagem').innerText=y.slip.advice
+    );
+    numero=proximoID
+});
+anterior.addEventListener('click',()=>{
+    let anteriorID = numero-1
+
+    fetch("https://api.adviceslip.com/advice/"+ anteriorID).then((promise)=>
     promise.json())
     .then((y)=>
     document.getElementById('mensagem').innerText=y.slip.advice
     );
+    numero=anteriorID
+});
