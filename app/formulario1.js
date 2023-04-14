732adf37876525dc6f72497aa31c0ddfaea2139c
brunomarques97 <116980642+brunomarques97@users.noreply.github.com>
2023-04-14 03:22:32 -0300

    Mundando html e o javascript

--- app/formulario1.js
+++ app/formulario1.js
@@ -1,42 +1,27 @@
-const min=1
-const max= 101
-let numero= Math.floor(Math.random() * (max-min))+min
-const proximo=document.querySelector('#proximo')
-const anterior=document.querySelector('#anterior')
 const procura=document.querySelector('#procura')
-const input=document.querySelector("input")
 
+fetch("https://api.adviceslip.com/advice").then((promise)=>
+    promise.json().then((res)=>(
+    document.getElementById('id').innerText="Advice"+res.slip.id
+)));
 
-fetch("https://api.adviceslip.com/advice/"+ numero).then((promise)=>
-    promise.json()).then((y)=>    
-    document.getElementById('mensagem').innerText=y.slip.advice
-    );
-
-proximo.addEventListener('click',()=>{
-    let proximoID = numero+1
-
-    fetch("https://api.adviceslip.com/advice/"+ proximoID).then((promise)=>
-    promise.json()).then((y)=>
-    document.getElementById('mensagem').innerText=y.slip.advice
-    );
-    numero=proximoID
-});
-anterior.addEventListener('click',()=>{
-    let anteriorID = numero-1
-
-    fetch("https://api.adviceslip.com/advice/"+ anteriorID).then((promise)=>
-    promise.json())
-    .then((y)=>
-    document.getElementById('mensagem').innerText=y.slip.advice
-    );
-    numero=anteriorID
-});
 procura.addEventListener('click',()=>{
-    let numerodigitado=input.value
-
-    fetch("https://api.adviceslip.com/advice/"+ numerodigitado).then((promise)=>
+    fetch("https://api.adviceslip.com/advice").then((promise)=>
     promise.json())
-    .then((y)=>
-    document.getElementById('mensagem').innerText=y.slip.advice
+    .then((res)=>
+    document.getElementById('id').innerText="Advice"+res.slip.id
+    );
+});
+
+fetch("https://api.adviceslip.com/advice").then((promise)=>
+    promise.json().then((res)=>(
+    document.getElementById('mensagem').innerText=res.slip.advice
+)));
+
+procura.addEventListener('click',()=>{
+    fetch("https://api.adviceslip.com/advice").then((promise)=>
+    promise.json())
+    .then((res)=>
+    document.getElementById('mensagem').innerText=res.slip.advice    
     );
 });
